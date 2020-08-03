1484619019f1a23df15ee1d1f187cff9eba5d3a7
Yoni-Gold <yonatan.r.gold@gmail.com>
2020-08-03 17:08:11 +0300

    img switches on hover

--- main.js
+++ main.js
@@ -28,9 +28,16 @@ function MakeDisplay(data)
   let t = document.createElement("table");
   t.innerHTML = `<tr><td> Name </td><td> ID </td><td> Info </td></tr>`; 
   t.innerHTML += `<tr><td> ${data.name} </td><td> ${data.id} </td><td> <ul> <li> Type: ${data.types.map(e => { return e.type.name })}</li><li> Height: ${data.height}</li><li> Weight: ${data.weight}</li> </ul> </td></tr>`;
-  t.innerHTML += `<tr><td><img id="pokeimg" src="${data.sprites.front_default}" width="100" height="100"></td><td><img id="pokeimg" src="${data.sprites.back_default}" width="100" height="100"></td></tr>`;
+  t.innerHTML += `<tr><td><img id="pokeimg" src="${data.sprites.front_default}" alt="${data.sprites.back_default}" width="100" height="100"></td></tr>`;
   document.getElementById("results").appendChild(t);
+  document.getElementsByTagName("img")[0].addEventListener("mouseover", switchImg);
   // pokeAnimation(data);
+}
+
+function switchImg(e)
+{
+  [e.currentTarget.src, e.currentTarget.alt] = [e.currentTarget.alt, e.currentTarget.src];
+  e.currentTarget.addEventListener("mouseleave", switchImg);
 }
 
 // async function pokeAnimation(data)
