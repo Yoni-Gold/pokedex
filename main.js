b8b8f5a5027ce2fc1d2a8cd86892377fb1b322e6
Yoni-Gold <yonatan.r.gold@gmail.com>
2020-08-03 18:09:40 +0300

    can see all pokemon from the same type

--- main.js
+++ main.js
@@ -6,6 +6,7 @@ const searchPokemon = async () => {
   {
     try {
       const { data } = await axios.get(`http://pokeapi.co/api/v2/pokemon/${txt.value}`);
+      console.log(data);
       MakeDisplay(data);
     }
     catch{
@@ -16,6 +17,11 @@ const searchPokemon = async () => {
   {
     window.alert("Enter a name or id");
   }
+  console.log(document.getElementsByClassName("type")[0]);
+
+  // document.getElementsByClassName("type")[0].addEventListener('click', getTypes);
+
+  Array.from(document.getElementsByClassName("type")).forEach(e => {e.addEventListener('click', getTypes)});
  
 };
 
@@ -27,11 +33,11 @@ function MakeDisplay(data)
   document.getElementById("results").firstChild ? document.getElementById("results").removeChild(document.getElementById("results").firstChild) : null;
   let t = document.createElement("table");
   t.innerHTML = `<tr><td> Name </td><td> ID </td><td> Info </td></tr>`; 
-  t.innerHTML += `<tr><td> ${data.name} </td><td> ${data.id} </td><td> <ul> <li> Type: ${data.types.map(e => { return e.type.name })}</li><li> Height: ${data.height}</li><li> Weight: ${data.weight}</li> </ul> </td></tr>`;
+  t.innerHTML += `<tr><td> ${data.name} </td><td> ${data.id} </td><td> <ul> <li> Type: ${printList(data.types)}</li><li> Height: ${data.height}</li><li> Weight: ${data.weight}</li> </ul> </td></tr>`;
   t.innerHTML += `<tr><td><img id="pokeimg" src="${data.sprites.front_default}" alt="${data.sprites.back_default}" width="100" height="100"></td></tr>`;
   document.getElementById("results").appendChild(t);
+  // document.getElementsByClassName("type").forEach(e => {e.addEventListener('click', getTypes)});
   document.getElementsByTagName("img")[0].addEventListener("mouseover", switchImg);
-  // pokeAnimation(data);
 }
 
 function switchImg(e)
@@ -40,12 +46,22 @@ function switchImg(e)
   e.currentTarget.addEventListener("mouseleave", switchImg);
 }
 
-// async function pokeAnimation(data)
-// {
-//   img = document.getElementById("pokeimg");
-//   for (let i = 0; i < 5; i++)
-//   {
-//     setTimeout(() => {img.src = data.sprites.front_default}, 1000);
-//     setTimeout(() => {img.src = data.sprites.back_default}, 1000);
-//   }
-// }
+function printList(arr)
+{
+  let result = arr.map(e => {return `<button class="type" id="${e.type.url}">${e.type.name}</button>`});
+
+  return result;
+}
+
+async function getTypes(e)
+{
+  const { data } = await axios.get(e.currentTarget.id);
+
+  let txt = " ";
+  let counter = 0;
+
+  data.pokemon.forEach(e => {counter++; txt += (`${counter} )  ${e.pokemon.name}
+  `)});
+
+  window.alert(txt);
+}
